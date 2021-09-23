6ffbcbb4adb7ad5e8bb36474e30882ac9dd8f3a7
Scott Bradley <scottabradley91@gmail.com>
2021-09-23 18:16:21 -0400

    added due-dates and momentjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,9 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -149,6 +152,10 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
 // task text was clicked
 $(".list-group").on("click", "p", function() {
   // get current text of p element
@@ -205,9 +212,37 @@ $(".list-group").on("click", "span", function() {
     .val(date);
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the `dateInput`
+      $(this).trigger("change");
+    }
+  })
+
   // automatically bring up the calendar
   dateInput.trigger("focus");
 });
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 
 // value of due date was changed
 $(".list-group").on("change", "input[type='text']", function() {
@@ -231,6 +266,9 @@ $(".list-group").on("change", "input[type='text']", function() {
     .addClass("badge badge-primary badge-pill")
     .text(date);
     $(this).replaceWith(taskSpan);
+  
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));  
 });
 
 // remove all tasks
